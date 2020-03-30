48abaefdb9aabc8d2753b90af2faa26fd5798ffc
alonsocastilleja <61435300+alonsocastilleja@users.noreply.github.com>
2020-03-30 11:16:55 -0400

    This is my challenge

---
+++ Array_Checkpoint/arrays.js
@@ -0,0 +1,20 @@
+/*********
+ * Three Javascript Array Methods
+ *   .shift()
+ *   .unshift()
+ *   .push()
+ *   .pop
+ *   .indexOf()
+ */
+
+
+
+var drinks = ['water', 'coffee', 'tea'];
+var foods = ['apple', 'sandwich', 'chips'];
+var meals = ['water', 'coffee', 'tea'];
+
+meals.push('apple');
+meals.push('sandwich');
+meals.push('chips');
+
+console.log(meals);
